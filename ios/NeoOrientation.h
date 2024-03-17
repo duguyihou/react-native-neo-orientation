17c648fe30d211eb01cf3e8e2e8b26dbd680c3bf
duguyihou <duguyihou@gmail.com>
2024-03-17 15:16:27 +1100

    3 drop support ios 12 (#4)
    * feat: drop iOS 12 support
    
    * refactor: interfaceOrientation
    
    * refactor: change interfaceOrientation to orientation
    
    * chore: fmt
    
    * refactor: fmt
    
    * fix: wip
    
    * chore: wip
    
    * ci: disable build iOS

--- ios/NeoOrientation.h
+++ ios/NeoOrientation.h
@@ -1,11 +1,3 @@
-//
-//  NeoOrientation.h
-//  NeoOrientation
-//
-//  Created by Kong Hou on 20/7/2023.
-//  Copyright © 2023 Facebook. All rights reserved.
-//
-
 #import <Foundation/Foundation.h>
 #import <React/RCTBridgeModule.h>
 #import <React/RCTEventEmitter.h>
--- ios/NeoOrientation.swift
+++ ios/NeoOrientation.swift
@@ -2,179 +2,160 @@ import Foundation
 
 @objc(NeoOrientation)
 class NeoOrientation: RCTEventEmitter {
+  private struct Constants {
+    static var orientationDidChange = "orientationDidChange"
+    static var deviceOrientationDidChange = "deviceOrientationDidChange"
+    static var lockDidChange = "lockDidChange"
+  }
   private var lastOrientation: UIInterfaceOrientation?
   private var lastDeviceOrientation: UIDeviceOrientation?
-  private var isLocking: Bool?
+  private var isLocking = false
   private static var orientationMask: UIInterfaceOrientationMask = .all
-  
+
+  private var deviceOrientation: UIDeviceOrientation {
+    return UIDevice.current.orientation
+  }
+
+  private var orientation: UIInterfaceOrientation? {
+    let windowScene = UIApplication.shared.windows.first?.windowScene
+    let orientation = windowScene?.interfaceOrientation
+    return orientation
+  }
+
   private func setOrientation(_ orientationMask: UIInterfaceOrientationMask) {
     NeoOrientation.orientationMask = orientationMask
   }
-  
+
   @objc
   static func getOrientation() -> UIInterfaceOrientationMask {
     return orientationMask
   }
-  
+
   override func supportedEvents() -> [String] {
-    return ["orientationDidChange", "deviceOrientationDidChange", "lockDidChange"]
+    return [
+      Constants.orientationDidChange,
+      Constants.deviceOrientationDidChange,
+      Constants.lockDidChange
+    ]
   }
-  
+
   override init() {
     super.init()
-    if #available(iOS 13.0, *) {
-      lastOrientation = UIApplication.shared.windows.first?.windowScene?.interfaceOrientation
-    } else {
-      lastOrientation = UIApplication.shared.statusBarOrientation
-    }
-    lastDeviceOrientation = getDeviceOrientation()
-    isLocking = false
-    
+    lastOrientation = orientation
+    lastDeviceOrientation = deviceOrientation
     NotificationCenter.default.addObserver(self,
                                            selector: #selector(deviceOrientationDidChange),
                                            name: UIDevice.orientationDidChangeNotification,
                                            object: nil)
-    addListener("orientationDidChange")
+    addListener(Constants.orientationDidChange)
   }
-  
+
   deinit {
     NotificationCenter.default.removeObserver(self)
     removeListeners(1)
   }
-  
-  
+
+
   @objc
   func getOrientation(callback: @escaping RCTResponseSenderBlock) {
-    OperationQueue.main.addOperation {
-      var orientation: UIInterfaceOrientation?
-      if #available(iOS 13.0, *) {
-        orientation = UIApplication.shared.windows.first?.windowScene?.interfaceOrientation
-      } else {
-        orientation = UIApplication.shared.statusBarOrientation
-      }
-      let orientationStr = self.getOrientationStr(orientation)
+    OperationQueue.main.addOperation { [self] in
+      let orientationStr = getOrientationStr(orientation)
       callback([orientationStr])
     }
   }
-  
+
   @objc
   func getDeviceOrientation(callback: @escaping RCTResponseSenderBlock) {
-    OperationQueue.main.addOperation {
-      let deviceOrientation = self.getDeviceOrientation()
-      let deviceOrientationStr = self.getDeviceOrientationStr(deviceOrientation)
+    OperationQueue.main.addOperation { [self] in
+      let deviceOrientationStr = getDeviceOrientationStr(deviceOrientation)
       callback([deviceOrientationStr])
     }
   }
-  
+
   @objc
   func lockToPortrait() {
-    OperationQueue.main.addOperation {
-      self.lockToOrientation(.portrait, withMask: .portrait)
+    OperationQueue.main.addOperation { [self] in
+      lockToOrientation(.portrait, withMask: .portrait)
     }
   }
-  
+
   @objc
   func lockToPortraitUpsideDown() {
-    OperationQueue.main.addOperation {
-      self.lockToOrientation(.portraitUpsideDown, withMask: .portraitUpsideDown)
+    OperationQueue.main.addOperation { [self] in
+      lockToOrientation(.portraitUpsideDown, withMask: .portraitUpsideDown)
     }
   }
-  
+
   @objc
   func lockToLandscape() {
     OperationQueue.main.addOperation { [self] in
       isLocking = true
-      let deviceOrientation = lastDeviceOrientation
-      var orientation: UIInterfaceOrientation?
-      if #available(iOS 13.0, *) {
-        orientation = UIApplication.shared.windows.first?.windowScene?.interfaceOrientation
-      } else {
-        orientation = UIApplication.shared.statusBarOrientation
-      }
       let orientationStr = getOrientationStr(orientation)
-      
+
       UIDevice.current.setValue(UIInterfaceOrientation.unknown.rawValue, forKey: "orientation")
-      
+
       if orientationStr == "landscapeRight" {
-        
+
         setOrientation(.landscape)
         UIDevice.current.setValue(UIInterfaceOrientation.landscapeLeft.rawValue, forKey: "orientation")
       } else {
         setOrientation(.landscape)
         UIDevice.current.setValue(UIInterfaceOrientation.landscapeRight.rawValue, forKey: "orientation")
       }
-      
-      UIDevice.current.setValue(deviceOrientation, forKey: "orientation")
+
+      UIDevice.current.setValue(lastDeviceOrientation, forKey: "orientation")
       UIViewController.attemptRotationToDeviceOrientation()
-      sendEvent(withName: "lockDidChange", body: ["orientation": "landscapeLeft"])
+      sendEvent(withName: Constants.lockDidChange, 
+                body: ["orientation": "landscapeLeft"])
       isLocking = false
     }
   }
-  
+
   @objc
   func lockToLandscapeRight() {
-    OperationQueue.main.addOperation {
-      self.lockToOrientation(.landscapeLeft, withMask: .landscapeLeft)
+    OperationQueue.main.addOperation { [self] in
+      lockToOrientation(.landscapeLeft, withMask: .landscapeLeft)
     }
   }
-  
+
   @objc
   func lockToLandscapeLeft() {
-    OperationQueue.main.addOperation {
-      self.lockToOrientation(.landscapeRight, withMask: .landscapeRight)
+    OperationQueue.main.addOperation { [self] in
+      lockToOrientation(.landscapeRight, withMask: .landscapeRight)
     }
   }
-  
+
   @objc
   func unlockAllOrientations() {
-    OperationQueue.main.addOperation {
-      self.lockToOrientation(.unknown, withMask: .all)
+    OperationQueue.main.addOperation { [self] in
+      lockToOrientation(.unknown, withMask: .all)
     }
   }
-  
+
   @objc
   override func constantsToExport() -> [AnyHashable : Any]! {
-    var orientation: UIInterfaceOrientation?
-    if #available(iOS 13.0, *) {
-      orientation = UIApplication.shared.windows.first?.windowScene?.interfaceOrientation
-    } else {
-      orientation = UIApplication.shared.statusBarOrientation
-    }
-    let orientationStr = getOrientationStr(orientation)
-    
-    return ["initialOrientation": orientationStr]
+    return ["initialOrientation": getOrientationStr(orientation)]
   }
-  
+
   override class func requiresMainQueueSetup() -> Bool {
     return true
   }
 }
 
 extension NeoOrientation {
-  private func getDeviceOrientation() -> UIDeviceOrientation {
-    return UIDevice.current.orientation
-  }
-  
   @objc
   private func deviceOrientationDidChange() {
-    var orientation: UIInterfaceOrientation?
-    if #available(iOS 13.0, *) {
-      let windowScene = UIApplication.shared.windows.first?.windowScene
-      
-      orientation = windowScene?.interfaceOrientation
-    } else {
-      orientation = UIApplication.shared.statusBarOrientation
-    }
-    let deviceOrientation = getDeviceOrientation()
     guard deviceOrientation != .unknown else { return }
-    
+
     if orientation != .unknown && orientation != lastOrientation {
-      sendEvent(withName: "orientationDidChange", body: ["orientation" : getOrientationStr(orientation)])
+      sendEvent(withName: Constants.orientationDidChange,
+                body: ["orientation" : getOrientationStr(orientation)])
       lastOrientation = orientation
     }
-    
-    if !isLocking! && deviceOrientation != lastDeviceOrientation {
-      sendEvent(withName: "deviceOrientationDidChange", body: ["orientation": getDeviceOrientationStr(deviceOrientation)])
+
+    if !isLocking && deviceOrientation != lastDeviceOrientation {
+      sendEvent(withName: Constants.deviceOrientationDidChange,
+                body: ["orientation": getDeviceOrientationStr(deviceOrientation)])
       lastDeviceOrientation = deviceOrientation
     }
   }
@@ -182,9 +163,8 @@ extension NeoOrientation {
     isLocking = true
     setOrientation(mask)
     let currentDevice = UIDevice.current
-    //    currentDevice.setValue(UIInterfaceOrientation.unknown, forKey: "orientation")
     currentDevice.setValue(newOrientation, forKey: "orientation")
-    
+
     if #available(iOS 16.0, *) {
       let windowScene = UIApplication.shared.connectedScenes.first as! UIWindowScene
       windowScene.requestGeometryUpdate(.iOS(interfaceOrientations: mask))
@@ -192,10 +172,11 @@ extension NeoOrientation {
     } else {
       UIViewController.attemptRotationToDeviceOrientation()
     }
-    sendEvent(withName: "lockDidChange", body: ["orientation": getOrientationStr(newOrientation)])
+    sendEvent(withName: Constants.lockDidChange,
+              body: ["orientation": getOrientationStr(newOrientation)])
     isLocking = false
   }
-  
+
   private func getOrientationStr(_ orientation: UIInterfaceOrientation?) -> String {
     switch orientation {
     case .portrait:
@@ -210,7 +191,7 @@ extension NeoOrientation {
       return "unknown"
     }
   }
-  
+
   private func getDeviceOrientationStr(_ orientation: UIDeviceOrientation?) -> String {
     switch orientation {
     case .portrait:
